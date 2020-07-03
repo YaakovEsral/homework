636f277e174454b2f3404c283e61860c2bbc28d2
Yaakov Esral <yaakovez@gmail.com>
2020-07-03 17:34:37 -0400

    Homework 53

---
+++ js/53console_prompt/index.js
@@ -0,0 +1,4 @@
+'use strict'
+
+let name = 'Yaakov';
+let email = 'yaakovez@gmail.com';
---
+++ js/53console_prompt/index2.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log('Name: ', name, '\n', 'Email: ', email);
+
+let age = prompt('What is your age?', '40');
+
+console.log('Your age: ', age);
+
+if(age>=21){
+    alert('So are you starting shidduchim?')
+};
+
+confirm('Do you really want to start?');
